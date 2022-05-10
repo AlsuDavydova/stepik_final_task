9fa63f445b0ea7ca5e9a6709dfe10bf0e9a6002d
AlsuDavydova <104736320+AlsuDavydova@users.noreply.github.com>
2022-05-10 04:21:17 +0300

    Update test_main_page.py

--- test_main_page.py
+++ test_main_page.py
@@ -6,12 +6,11 @@ from .pages.basket_page import BasketPage
 
 @pytest.mark.login_guest
 class TestLoginFromMainPage():
-    # не забываем передать первым аргументом self  
     def test_guest_can_go_to_login_page(self, browser):
         link = "http://selenium1py.pythonanywhere.com/"
-        page = BasePage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
-        page.open()                      # открываем страницу
-        page.should_be_login_page()          # выполняем метод страницы — переходим на страницу логина
+        page = BasePage(browser, link)  
+        page.open()
+        page.should_be_login_page()          
         login_page = LoginPage(browser, browser.current_url)
         login_page.should_be_login_page()
     
