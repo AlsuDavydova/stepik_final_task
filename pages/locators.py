74d11bebf8603d563b405250630c1990a6762407
AlsuDavydova <104736320+AlsuDavydova@users.noreply.github.com>
2022-05-10 04:23:22 +0300

    Update locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -20,7 +20,6 @@ class ObjectPageLocators():
 
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
-    #LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
     USER_ICON = (By.CSS_SELECTOR, ".icon-user")
 
 class BasketPageLocators():
